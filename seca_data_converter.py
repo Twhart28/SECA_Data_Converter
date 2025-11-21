a23e8dc3243749c6745fb2e26cf5d7b50175c9ba
Twhart28 <Thomaswhart28@gmail.com>
2025-11-20 18:50:32 -0600

    Changed tolerances

--- seca_data_converter.py
+++ seca_data_converter.py
@@ -263,7 +263,7 @@ def evaluate_data_quality(values: Dict[str, Optional[float]]) -> Dict[str, Optio
         fmi = values["Fat Mass Index (kg/m^2)"]
         ffmi = values["Fat-Free Mass Index (kg/m^2)"]
         bmi = values["Body Mass Index (kg/m^2)"]
-        if not almost_equal((fmi or 0) + (ffmi or 0), bmi or 0, 0.01):
+        if not almost_equal((fmi or 0) + (ffmi or 0), bmi or 0, 0.2):
             failures.append("3")
     else:
         failures.append("3")
@@ -286,7 +286,7 @@ def evaluate_data_quality(values: Dict[str, Optional[float]]) -> Dict[str, Optio
                 "Torso (kg)",
             ]
         )
-        if not almost_equal(sum_limbs, values["Skeletal Muscle Mass (kg)"] or 0, 0.01):
+        if not almost_equal(sum_limbs, values["Skeletal Muscle Mass (kg)"] or 0, 0.3):
             failures.append("4")
     else:
         failures.append("4")
@@ -297,7 +297,7 @@ def evaluate_data_quality(values: Dict[str, Optional[float]]) -> Dict[str, Optio
         bmi = values["Body Mass Index (kg/m^2)"]
         if height in (0, None):
             failures.append("5")
-        elif not almost_equal((weight or 0) / ((height or 1) ** 2), bmi or 0, 0.01):
+        elif not almost_equal((weight or 0) / ((height or 1) ** 2), bmi or 0, 0.4):
             failures.append("5")
     else:
         failures.append("5")
@@ -312,7 +312,7 @@ def evaluate_data_quality(values: Dict[str, Optional[float]]) -> Dict[str, Optio
         ratio = values["ECW/TBW (%)"]
         if tbw in (0, None):
             failures.append("6")
-        elif not almost_equal(((ecw or 0) / (tbw or 1)) * 100, ratio or 0, 0.01):
+        elif not almost_equal(((ecw or 0) / (tbw or 1)) * 100, ratio or 0, 0.2):
             failures.append("6")
     else:
         failures.append("6")
@@ -327,7 +327,7 @@ def evaluate_data_quality(values: Dict[str, Optional[float]]) -> Dict[str, Optio
         ratio = values["ECW/TBW (%)"]
         if tbw_pct in (0, None):
             failures.append("7")
-        elif not almost_equal(((ecw_pct or 0) / (tbw_pct or 1)) * 100, ratio or 0, 0.01):
+        elif not almost_equal(((ecw_pct or 0) / (tbw_pct or 1)) * 100, ratio or 0, 0.2):
             failures.append("7")
     else:
         failures.append("7")
